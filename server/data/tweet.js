d25dbb9ec5ba9cb15f222d60f69198b17c181966
ojiwoong <ojiwoong@gmail.com>
2022-03-16 15:19:52 +0900

    서버 컨틀롤러, 데이터 모델 async 비동기 처리
    * server/controller/tweet.js, server/data/tweet.js : export function => export async function 으로 수정

--- server/data/tweet.js
+++ server/data/tweet.js
@@ -17,19 +17,19 @@ let tweets = [
   },
 ];
 
-export function getAll() {
+export async function getAll() {
   return tweets;
 }
 
-export function getAllByUsername(username) {
+export async function getAllByUsername(username) {
   return tweets.filter(tweet => tweet.username === username);
 }
 
-export function getAllById(id) {
+export async function getAllById(id) {
   return tweets.find(tweet => tweet.id === id);
 }
 
-export function create(text, name, username) {
+export async function create(text, name, username) {
   const tweet = {
     id: Date.now().toString(),
     text,
@@ -41,7 +41,7 @@ export function create(text, name, username) {
   tweets = [tweet, ...tweets];
 }
 
-export function update(id, text) {
+export async function update(id, text) {
   const tweet = tweets.find(tweet => tweet.id === id);
   if (tweet) {
     tweet.text = text;
@@ -50,6 +50,6 @@ export function update(id, text) {
   return tweet;
 }
 
-export function remove(id) {
+export async function remove(id) {
   const tweet = tweets.filter(tweet => tweet.id !== id);
 }
